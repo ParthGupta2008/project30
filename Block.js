cf444baa91d0ceed4ef92f1c90659acf02d4d962
ParthGupta2008 <78309697+ParthGupta2008@users.noreply.github.com>
2021-05-30 16:38:31 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,38 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.image = loadImage("block.png")
+      World.add(world, this.body);
+    }
+    display(){
+      
+      //rectMode(CENTER);
+      //rect(this.body.position.x,this.body.position.y,this.body.width, this.body.height);
+      push();
+      translate(this.body.position.x, this.body.position.y);
+
+      imageMode(CENTER);
+      image(this.image, 0, 0, this.width, this.height);
+      
+      pop()
+      
+      if(this.body.speed>3){
+        World.remove(world,this.body)
+        push()
+
+        this.Visiblity= this.Visiblity-5
+        tint (255,this.Visiblity)
+        image(this.image, 0, 0, this.width, this.height);
+        pop ();
+      }
+      else{
+        super.display()
+      }
+    }
+}
